41e5bcde0068ca99b7fbe799222ea86d1f318782
Edwin Li <edwin.li@outlook.com>
2026-05-10 20:39:31 -0400

    feat: connected website to personal domain name

--- next.config.ts
+++ next.config.ts
@@ -5,8 +5,6 @@ const isProd = process.env.NODE_ENV === 'production';
 const nextConfig: NextConfig = {
   output: "export",
   reactStrictMode: true,
-  basePath:  isProd ? "/edwin-li" : "",
-  assetPrefix: isProd ? "/edwin-li/" : "",
   images: {
     unoptimized: true
   }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,11 @@
 import Image from "next/image";
-import nextConfig from "../../next.config";
 
 export default function Home() {
   return (
     <div className="flex flex-row justify-center">
       <div className="flex flex-col">
         <Image
-          src={`${nextConfig.basePath}/headshot.svg`}
+          src={"/headshot.svg"}
           alt="headshot"
           width={200}
           height={200}
@@ -15,13 +14,13 @@ export default function Home() {
 
         <h2>hello, my name is</h2>
         <h1>Edwin Li</h1>
-        <h3>Currently studying ECE @ CMU</h3>
+        <h3>Currently studying ECE @ CMU!</h3>
 
         <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
           <a href="https://github.com/Hipposhark" target="_blank" rel="noopener noreferrer">
             <Image
               aria-hidden
-              src={`${nextConfig.basePath}/github_icon.svg`}
+              src={"/github_icon.svg"}
               alt="gitHub icon"
               width={24}
               height={24}
@@ -30,7 +29,7 @@ export default function Home() {
           <a href="https://www.linkedin.com/in/edwin-li-5a398b248/" target="_blank" rel="noopener noreferrer">
             <Image
               aria-hidden
-              src={`${nextConfig.basePath}/linkedin_icon.svg`}
+              src={"/linkedin_icon.svg"}
               alt="linkedin icon"
               width={24}
               height={24}
